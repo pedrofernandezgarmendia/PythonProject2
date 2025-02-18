1f135e79d650d05e394a62587de3cafb4765c12f
pedrofernandez <pedrofernandezgarmendia@gmail.com>
2025-02-18 21:21:08 +0100

    multiplesof6exercises

--- multipleof6.py
+++ multipleof6.py
@@ -1,18 +1,17 @@
 def get_multiple_6():
     """
     Returns a multiple of 6 that was entered by the user
-    :return: int a number
+    :return: int - a number that is a multiple of 6.
     """
     while True:
         try:
-            n = input("Please give me a multiple of 6: ")
-            n = input(n) # convert to int
+            n = int(input("Please give me a multiple of 6: "))
             # how to check if it is a multiple of 6
-            if n / 6 == n // 6:
+            if n % 6 == 0:
                 return n
             else:
-            print("that is not a multiple of 6")
-    except ValueError:
-        print("you have not entered a number")
+                print("that is not a multiple of 6. Try again")
+        except ValueError:
+            print("you have not entered a number. Try again")
 
 print(get_multiple_6())
